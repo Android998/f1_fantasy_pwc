ef96a6466052361685a09abe2572fff289543aa3
agurtubay <agurtubay@microsoft.com>
2026-03-04 10:02:22 +0100

    Update media URL references in templates and add media context processor in settings

--- f1porra_website/settings.py
+++ f1porra_website/settings.py
@@ -111,6 +111,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'f1porra_website.apps.accounts.context_processors.current_user_profile',
